69a038926a7fa4ae023d7fb9af52ce7e45dfa370
Jorge Escalante <hi@jescalante.me>
2022-08-17 22:18:34 -0600

    Divide el proyecto de marvel en bloques de código

--- marvel-api/main.py
+++ marvel-api/main.py
@@ -1,26 +1,67 @@
 import hashlib
 import requests
 import time
+import math
 
+# Autenticación para la API
 endpoint = 'https://gateway.marvel.com/v1/public/'
 resource = 'characters'
 timestamp = time.time()
 public_key = '1aa848defd6e3980f0a4ddd5c01af610'
 secret_key = 'd119b3af5741101a8ce81a010be86cb7ca6e2ca8'
-
 access = f"{timestamp}{secret_key}{public_key}"
 ultra_secret = hashlib.md5(access.encode())
 
-authentication = {
-    "apikey": public_key,
-    "ts": timestamp,
-    "hash": ultra_secret.hexdigest()
-}
+# Variables de control
+total_pages = 0
 
-response = requests.get(f"{endpoint}{resource}", params=authentication)
 
-r = response.json()
-results = r["data"]["results"]
-total = r["data"]["count"]
-for character in results:
-    print(character["name"])
+def get_characters(page, limit):
+    global total_pages
+
+    params = {
+        "apikey": public_key,
+        "ts": timestamp,
+        "hash": ultra_secret.hexdigest(),
+        "limit": limit,
+        "offset": page * limit
+    }
+
+    response = requests.get(f"{endpoint}{resource}", params=params)
+    data = response.json()["data"]
+    total = data["total"]
+    total_pages = math.ceil(total / limit)
+    characters = data["results"]
+
+    return characters
+
+
+def main():
+    page = 0
+    limit = 20
+    characters = get_characters(page, limit)
+    while True:
+        print('Personajes:')
+        for character in characters:
+            print(character["name"])
+        print(f"Página {page} de {total_pages}")
+        print('1- Página siguiente')
+        print('2- Página anterior')
+        print('3- Página en específico')
+        print('4- Salir')
+        option = int(input('Ingrese una opción: '))
+
+        if option == 1:
+            page = page + 1
+            characters = get_characters(page, limit)
+        elif option == 2:
+            pass
+        elif option == 3:
+            pass
+        elif option == 4:
+            break
+        else:
+            continue
+
+
+main()
